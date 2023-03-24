e38fa795548762a06c91a5356cbda11202506e4b
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-24 15:32:07 +0100

    push

--- insertion.php
+++ insertion.php
@@ -1,22 +1,20 @@
 <?php
-$NAME = $_GET['nom'];
+$NOM = $_POST['nom'];
 
-// conneexionn baseee de données 
+// Connexio à la base de données
 $BASE = new PDO('mysql:host=localhost; dbname=id20205701_samy', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
+$BASE->exec("SET CHARACTER SET utf8");
 
-$base->exec("SET CHARACTER SET utf8");
+// Préparation de la requête d'insertion
+$sql = "INSERT INTO users (nom) VALUES (:nom)";
+$stmt = $base->prepare($sql);
+$stmt->bindParam(':nom', $NOM);
 
-$sql = "INSERT INTO personne VALUES(NULL,'$NAME')";
-
-$result = $base->query($sql);
-
-if ($result == false) { 
-    echo '<span style="color:red"><strong>'.$NAME.'</strong> n\'a pas pu être ajouté dans la BDD.</span>'; 
-} 
-else {
-    echo '<span style="color:green"><strong>'.$NAME.'</strong> a été ajouté dans la BDD.</span>';
+// Exécution de la requête
+if ($stmt->execute()) {
+    echo '<span style="color:green"><strong>'.$NOM.'</strong> a été ajouté dans la BDD.</span>';
+} else {
+    echo '<span style="color:red"><strong>'.$NOM.'</strong> n\'a pas pu être ajouté dans la BDD.</span>';
 }
-    
 
 ?>
-
