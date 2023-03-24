f7e482388f3b051389e671bd84776f04c2cba3cb
SamyOhr <69979570+SamyOhr@users.noreply.github.com>
2023-03-24 13:05:22 +0100

    push

--- insertion.php
+++ insertion.php
@@ -1,5 +1,5 @@
 <?php
-$nom = $_GET['nom'];
+$NOM = $_GET['nom'];
 
 // Connexion à la base de données
 $base = new PDO('mysql:host=localhost;dbname=id20205701_samy;charset=utf8', 'id20205701_samyouicher', '/&*hX18M$A}2#QGr');
@@ -7,12 +7,12 @@ $base = new PDO('mysql:host=localhost;dbname=id20205701_samy;charset=utf8', 'id2
 // Préparation de la requête d'insertion
 $sql = "INSERT INTO personne (nom) VALUES (:nom)";
 $stmt = $base->prepare($sql);
-$stmt->bindParam(':nom', $nom);
+$stmt->bindParam(':nom', $NOM);
 
 // Exécution de la requête
 if ($stmt->execute()) {
-    echo '<span style="color:green"><strong>'.$nom.'</strong> a été ajouté dans la BDD.</span>';
+    echo '<span style="color:green"><strong>'.$NOM.'</strong> a été ajouté dans la BDD.</span>';
 } else {
-    echo '<span style="color:red"><strong>'.$nom.'</strong> n\'a pas pu être ajouté dans la BDD.</span>';
+    echo '<span style="color:red"><strong>'.$NOM.'</strong> n\'a pas pu être ajouté dans la BDD.</span>';
 }
 ?>
